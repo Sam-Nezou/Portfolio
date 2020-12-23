ed1aea6fc8ac17f58062cc8c32bbbda75bed51a5
Samuel <sam.nezou@gmail.com>
2020-12-23 22:44:29 +0100

    Finalisation Résumé 	- Faire une meilleur description

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-resume',
@@ -10,9 +11,18 @@ export class ResumeComponent implements OnInit {
 
   imgSource ="../../assets/images/pp.jpeg";
   text_resume = "Etudiant à l'ISEN M1 - Ecole de Haute Technologie et Ingénierie Numérique. Je suis à la recherche d'un stage technique de 3 mois à partir du 3 mai 2021 et d'une alternance à la suite de celui-ci. "
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+  
+
 
   ngOnInit(): void {
   }
+
+  open(content) {
+    this.modalService.open(content, {size:'xl'});
+  }
+
+  
+
 
 }
