c542d4f40f749dfde1cd5de4eb12712230c4ce01
Samuel <sam.nezou@gmail.com>
2020-12-23 11:38:53 +0100

    Quelque modif

--- src/app/license/license.component.ts
+++ src/app/license/license.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
+class LicenseUser {
+
+  author: string;
+  authorSource: string;
+  siteSource: string;
+  site: string;
+
+  constructor(author: string, authorSource: string, siteSource: string, site:string) {
+    this.author = author;
+    this.authorSource = authorSource;
+    this.siteSource = siteSource;
+    this.site = site;
+  }
+
+}
+
+
 @Component({
   selector: 'app-license',
   templateUrl: './license.component.html',
@@ -11,5 +31,12 @@ export class LicenseComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+
+  LicenseUsers : Array<LicenseUser> = [
+    new LicenseUser("Freepik","https://www.flaticon.com/fr/auteurs/freepik","https://www.flaticon.com","www.flaticon.com"),
+    new LicenseUser("Freepik","https://www.flaticon.com/fr/auteurs/freepik","https://www.flaticon.com","www.flaticon.com")
+  ]
 
 }
